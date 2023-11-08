3d4fb273f7505f028eecf6f9fb5b7169535adfa4
konung-yaropolk <yaropolk1995@gmail.com>
2023-11-08 17:59:45 -0600

    new file:   A_0007.txt 	modified:   ImageJ-csv-cutter.py

--- ImageJ-csv-cutter.py
+++ ImageJ-csv-cutter.py
@@ -9,13 +9,13 @@ TIME_AFTER_TRIG  = 30    # in sec.
 DIRECTORIES = [
     # 'C:/Users/yarop/Coding/ImageJ-csv-cutter/',
      'F:/Lab Work Files/scripts/ImageJ-csv-cutter/',
-    'F:/Lab Work Files/2-photon/Pirt GCamp3 x Thy1 RGeco + DRS + Bicuculine/2022_12_09/',
+    # 'F:/Lab Work Files/2-photon/Pirt GCamp3 x Thy1 RGeco + DRS + Bicuculine/2022_12_09/',
     # 'F:/Lab Work Files/2-photon',
     
 ]
 
 
-def parser(path, file):    
+def parse(path, file):    
     file_path = path + file
     events = []
 
@@ -25,6 +25,7 @@ def parser(path, file):
         strings = file.readlines()
 
         for i, line in enumerate(strings):
+
             if trigger in line:
                 events.append([strings[i+1][18:-2], float(strings[i+2][15:-6])])
 
@@ -36,6 +37,7 @@ def file_finder(path, pattern):
 
     # Walk through the directory and its subdirectories
     for root, _, files in os.walk(path):
+
         for filename in files:
             #if file.endswith('.txt') and not file.startswith('!'):
             if re.search(pattern, filename):
@@ -47,10 +49,8 @@ def file_finder(path, pattern):
 def file_lister(path, pattern):
     files = []
 
-    if os.path.isdir(path):
-        
-        files.extend(file_finder(path, pattern))
-        
+    if os.path.isdir(path):        
+        files.extend(file_finder(path, pattern))        
         # if not files:
         #     print("No files found in the: ", path)
 
@@ -59,38 +59,41 @@ def file_lister(path, pattern):
     
     return files
 
+#def csv_transforming(patch, file, metadata):
 
-#def get_csv_list(metadata_list):
+
+def process_csv(path, file, metadata):
+
+    csv_list = []
+    csv_list.extend(file_lister(path, r'^' + file + r'.*\.csv$'))
+
+    if csv_list != []:
+        for patch, file in csv_list:
+            #csv_transforming(patch, file, metadata)
+            print(patch+file)
+
+    #print('\n',csv_list, '\n')    
+    csv_list = None
 
 
 def main():
 
-    metadata_list = []
+    queue = []
 
+    # walk thrue directories to add files to the queue 
     for dir in DIRECTORIES:
-        metadata_list.extend(file_lister(dir, r'^[^!].*\.txt$'))
+        queue.extend(file_lister(dir, r'^[^!].*\.txt$'))
 
-    print(metadata_list)
+    # append metadata to the queue
+    for i, elem in enumerate(queue):
+        metadata = parse(elem[0], elem[1])
+        queue[i].append(metadata)
 
-    csv_list = []
+    print(queue)
 
-    for path, file in metadata_list:
-        metadata = parser(path, file)
-        #print(metadata)
+    for path, file, metadata in queue:
+        process_csv(path, file, metadata)
 
-        csv_list.append(file_lister(path, r'^' + file + r'.*\.csv$'))
-
-        # if csv_list:
-        #     return csv_list
-        # else:
-        #     return []
-
-    print(csv_list)
-
-
-
-    
-        
         
 
 
