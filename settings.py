9cc762ea542f5bfc423d025c5d18ae4dd5bf37ee
First Last <yaropolk1995@gmail.com>
2023-11-17 23:54:30 -0600

    modified:   settings.py

--- settings.py
+++ settings.py
@@ -20,14 +20,14 @@ List below directories containing data (tiff + txt), then simple run the script.
 
 '''
 
-TIME_BEFORE_TRIG = 0    # time in sec, 0 - full trace
-TIME_AFTER_TRIG  = 0    # time in sec, 0 - full trace
+TIME_BEFORE_TRIG = 10    # time in sec, 0 - full trace
+TIME_AFTER_TRIG  = 30    # time in sec, 0 - full trace
 
 RELATIVE_VALUES = True   # dF/F0 output format
 
 DIRECTORIES = [
     # 'C:\\Users\\Lenin\\coding\\ImageJ-csv-cutter',
-     'F:/Lab Work Files/2-photon/Pirt_GCamp3 x MCU-KO + DRS + Caps/',
+    # 'F:/Lab Work Files/2-photon/Pirt_GCamp3 x MCU-KO + DRS + Caps/',
     # 'F:/Lab Work Files/2-photon/Microglia + C5a/',
     # 'F:/Lab Work Files/2-photon/Pirt GCamp3 x Thy1 RGeco SNI or SHAM + DRS  + PMX205 + Bicuculine/',
     
